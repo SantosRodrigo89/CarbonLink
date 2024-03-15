3d3b1edc03dbf79e093ee4601da57746a88a8ad2
SantosRodrigo89 <rodrigo.v.santos89@gmail.com>
2024-03-15 10:26:42 -0300

    feat: implementando wallet terceirocards

--- app/src/main/java/br/com/fiap/carbonlink/screens/WalletScreen.kt
+++ app/src/main/java/br/com/fiap/carbonlink/screens/WalletScreen.kt
@@ -34,7 +34,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import br.com.fiap.carbonlink.R
 
 @SuppressLint("InvalidColorHexValue")
