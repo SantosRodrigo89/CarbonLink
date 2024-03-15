f4b49b2e7c91452142811f6ef4a1d76840cca642
SantosRodrigo89 <rodrigo.v.santos89@gmail.com>
2024-03-15 10:21:48 -0300

    feat: implementando wallet terceirocards

--- app/src/main/java/br/com/fiap/carbonlink/screens/WalletScreen.kt
+++ app/src/main/java/br/com/fiap/carbonlink/screens/WalletScreen.kt
@@ -1,40 +1,217 @@
 package br.com.fiap.carbonlink.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import br.com.fiap.carbonlink.R
+import kotlin.math.round
 
+@SuppressLint("InvalidColorHexValue")
 @Composable
 fun WalletScreen(navController: NavController) {
     Box(modifier = Modifier
         .fillMaxSize()
-        .background(Color(0xFFED145B))
-        .padding(32.dp)
+        .background(Color.White)
+        .padding(16.dp)
     ){
-        Text(
-            text = "WALLET",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
-        Button(
-            onClick = { navController.navigate("menu") },
-            colors = ButtonDefaults.buttonColors(Color.White),
-            modifier = Modifier.align(Alignment.Center)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "VOLTAR", fontSize = 20.sp, color = Color.Blue)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text(
+                    text = "Carteira",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = colorResource(id = R.color.verde_app)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    Icons.Outlined.Notifications,
+                    contentDescription = "Notificação",
+                    Modifier
+                        .size(40.dp)
+                        .background(
+                            color = Color.LightGray,
+                            shape = RoundedCornerShape(50.dp)
+                        )
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedCard(
+                colors = CardDefaults.cardColors(
+                    containerColor = colorResource(id = R.color.verde_card)
+                ),
+                border = BorderStroke(1.dp, Color.Transparent),
+                elevation = CardDefaults.cardElevation(4.dp) ,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "logo",
+                    modifier = Modifier.size(60.dp),
+                    contentScale = ContentScale.Fit
+                )
+                Text(
+                    text = "Saldo de Carbono",
+                    modifier = Modifier
+                        .padding(16.dp, 4.dp, 0.dp, 0.dp),
+                    style = TextStyle(
+                        fontFamily = FontFamily.Monospace,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Normal,
+                        lineHeight = 19.sp,
+                        letterSpacing = 0.sp,
+                        textAlign = TextAlign.Left,
+                        color = Color(0xFF40513B)
+                    ),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "200.000854716",
+                    modifier = Modifier
+                        .padding(16.dp, 2.dp, 0.dp, 2.dp),
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        fontFamily = FontFamily.Default,
+                        fontSize = 26.sp,
+                        fontWeight = FontWeight.Bold,
+                        lineHeight = 35.sp,
+                        letterSpacing = 0.sp,
+                        textAlign = TextAlign.Left,
+                        color = Color(0xFF40513B)
+                    ),
+                )
+                Text(
+                    text = "= R$ 2.000,00",
+                    modifier = Modifier
+                        .padding(16.dp, 0.dp, 0.dp, 16.dp),
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        fontFamily = FontFamily.Default,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Light,
+                        lineHeight = 18.sp,
+                        letterSpacing = 0.sp,
+                        textAlign = TextAlign.Left,
+                        color = Color(0xFF40513B)
+                    ),
+                )
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedCard(
+                border = BorderStroke(1.dp, Color.Transparent),
+                elevation = CardDefaults.cardElevation(2.dp) ,
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                Text(
+                    text = "Chave de transferência",
+                    modifier = Modifier
+                        .padding(16.dp, 16.dp, 0.dp, 4.dp),
+                    style = TextStyle(
+                        fontFamily = FontFamily.Monospace,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Normal,
+                        lineHeight = 19.sp,
+                        letterSpacing = 0.sp,
+                        textAlign = TextAlign.Left,
+                        color = Color(0xFF40513B)
+                    ),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "SDAGhsdhaiwbJHSDFhdsajLKFDAwNasdXb",
+                    modifier = Modifier
+                        .padding(16.dp, 2.dp, 0.dp, 16.dp),
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        fontFamily = FontFamily.Default,
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.Light,
+                        lineHeight = 35.sp,
+                        letterSpacing = 0.sp,
+                        textAlign = TextAlign.Left,
+                        color = Color(0xFF40513B)
+                    ),
+                )
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedCard(
+                border = BorderStroke(1.dp, Color.Transparent),
+                elevation = CardDefaults.cardElevation(2.dp) ,
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                Text(
+                    text = "Histórico",
+                    modifier = Modifier
+                        .padding(16.dp, 16.dp, 0.dp, 4.dp),
+                    style = TextStyle(
+                        fontFamily = FontFamily.Monospace,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        lineHeight = 19.sp,
+                        letterSpacing = 0.sp,
+                        textAlign = TextAlign.Left,
+                        color = Color(0xFF40513B)
+                    ),
+                    textAlign = TextAlign.Center
+                )
+            }
         }
+//        Spacer(modifier = Modifier.height(24.dp))
+//        Button(
+//            onClick = { navController.navigate("menu") },
+//            colors = ButtonDefaults.buttonColors(Color.Gray),
+//            modifier = Modifier
+//                .align(Alignment.Center)
+//        ) {
+//            Text(text = "VOLTAR", fontSize = 20.sp, color = Color.Blue)
+//        }
     }
 }
+
+
